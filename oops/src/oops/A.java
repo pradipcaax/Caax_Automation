3e2fc5d5dec6b722ee4a0c178a0635295242c4ef
test <test@TESTING-PC>
2018-06-07 12:53:29 +0530

    all

---
+++ oops/src/oops/A.java
@@ -0,0 +1,24 @@
+package oops;
+
+class A
+{
+ A()
+ {
+  this("PRadip");
+ }
+ A(String x)
+ {
+  System.out.println("x="+x);
+ }
+ 
+ 
+ public void voidrunn() {
+	 
+	 System.out.println("testttts");
+	 
+ }
+public static void main(String arg[])
+{
+A object = new A();
+}
+}
