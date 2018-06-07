f161597f5e24e08e8fd30a9edbd8d73664ff10e7
test <test@TESTING-PC>
2018-06-07 13:03:22 +0530

    Hello test 1

---
+++ Test_Project_001/src/one.java
@@ -0,0 +1,9 @@
+
+public class one {
+
+	public static void main(String[] args) {
+	System.out.println("Hi i'm class one");
+
+	}
+
+}
---
+++ Test_Project_001/src/two.java
@@ -0,0 +1,8 @@
+
+public class two {
+
+	public static void main(String[] args) {
+		System.out.println("Hi I'm Class two");
+	}
+
+}
